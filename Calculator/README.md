dde1772d18b658b7e514731ddda311a2735f0412
Vitalii Levko <vlevko@student.unit.ua>
2019-06-13 11:51:33 +0300

    use Vue.js SFC

---
+++ Calculator/README.md
@@ -0,0 +1,13 @@
+# Calculator
+Basic calculator app with Vue.js
+
+## Prerequisites
+* Node.js 10.15.3
+* npm 6.4.1
+
+## Getting Started
+```bash
+cd Calculator
+npm install
+npm run dev
+```
--- Calculator/src/App.vue
+++ Calculator/src/App.vue
@@ -1,23 +1,98 @@
-Vue.component('control', {
-  props: ['func', 'turn'],
-  template: '<button v-on:click="func()">{{ turn }}</button>',
-})
+<template>
+  <div>
+    <div id="hello">
+      <h2>{{ msg }}</h2>
+    </div>
 
-var app = new Vue({
-  el: '#app',
+    <div id="calc-app" class="container">
+      
+      <div class="row justify-content-center">
+        <div class="center-block text-center">
+          <template v-if="turn === 'On'">
+            <input v-model="txt"
+              disabled 
+              placeholder="Calculator"
+            >    
+          </template>
+          <template v-else>
+            <input v-model="txt"
+              readonly
+              placeholder="0"
+            > 
+          </template>
+          <button v-on:click="trn">{{ turn }}</button>
+        </div>
+      </div>
 
-  data: {
-    txt: '',
-    one: '',
-    two: '',
-    op: null,
-    toOp: false,
-    toClr: false,
-    fromOp: false,
-    turn: 'On',
-    clsBtn: 'col-1 btn btn-light',
+      <div v-if="turn === 'Off'">
+        <div class="row justify-content-center">
+          <span v-on:click="clr()" v-bind:class="clsBtn">C</span>
+          <span v-on:click="bck" v-bind:class="clsBtn">←</span>
+          <span v-on:click="sqr()" v-bind:class="clsBtn">√</span>
+          <span v-on:click="div(false)" v-bind:class="clsBtn">÷</span>
+        </div>
+        <div class="row justify-content-center">
+          <span v-on:click="put(7)" v-bind:class="clsBtn">7</span>
+          <span v-on:click="put(8)" v-bind:class="clsBtn">8</span>
+          <span v-on:click="put(9)" v-bind:class="clsBtn">9</span>
+          <span v-on:click="mul(false)" v-bind:class="clsBtn">*</span>
+        </div>
+        <div class="row justify-content-center">
+          <span v-on:click="put(4)" v-bind:class="clsBtn">4</span>
+          <span v-on:click="put(5)" v-bind:class="clsBtn">5</span>
+          <span v-on:click="put(6)" v-bind:class="clsBtn">6</span>
+          <span v-on:click="sub(false)" v-bind:class="clsBtn">-</span>
+        </div>
+        <div class="row justify-content-center">
+          <span v-on:click="put(1)" v-bind:class="clsBtn">1</span>
+          <span v-on:click="put(2)" v-bind:class="clsBtn">2</span>
+          <span v-on:click="put(3)" v-bind:class="clsBtn">3</span>
+          <span v-on:click="sum(false)" v-bind:class="clsBtn">+</span>
+        </div>
+
+        <div class="row justify-content-center">
+          <span
+            v-for="btn in btns"
+            v-on:click="btn.act(btn.value)"
+            v-bind:class="btn.cls"
+          >{{ btn.title }}</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      msg: 'Welcome to Vue.js Calculator app',
+      btns: [
+        { id: 1, title: '±', act: 'inv', value: '', cls: 'col-1 btn btn-light' },
+        { id: 2, title: '0', act: 'put', value: '0', cls: 'col-1 btn btn-light' },
+        { id: 3, title: '.', act: 'put', value: '.', cls: 'col-1 btn btn-light' },
+        { id: 4, title: '=', act: 'res', value: '', cls: 'col-1 btn btn-light' },
+      ],
+      txt: '',
+      one: '',
+      two: '',
+      op: null,
+      toOp: false,
+      toClr: false,
+      fromOp: false,
+      turn: 'On',
+      clsBtn: 'col-1 btn btn-light',
+    }
   },
-
+  
+  created() {
+    this.btns.forEach(btn => { btn.act = this[btn.act] })
+  },
+  
   methods: {
 
     trn: function () {
@@ -119,7 +194,7 @@ var app = new Vue({
       this.fromOp = true
     },
 
-    inv: function () {
+    inv: function (arg) {
       if (this.txt) {
         this.txt = String(parseFloat(this.txt) * -1)
         if (!this.fromOp && !this.two) {
@@ -170,7 +245,7 @@ var app = new Vue({
       this.fromOp = true
     },
 
-    res: function () {
+    res: function (arg) {
       if (this.op) {
         this.toOp = true
         if (!this.two) {
@@ -181,5 +256,14 @@ var app = new Vue({
         this.toClr = true
       }
     },
-  }
-})
+  },
+}
+</script>
+
+<style>
+#hello {
+  font-family: Verdana;
+  color: navy;
+  text-align: center;
+}
+</style>
